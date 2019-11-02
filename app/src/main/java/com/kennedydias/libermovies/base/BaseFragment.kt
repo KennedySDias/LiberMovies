aa6dc33f742436c2e9bd0e48534261c94c7b408a
KennedyDias <kennedysouzadias@gmail.com>
2019-11-02 07:41:35 -0300

    Create the base views
    Creating the base activity and base fragment to set the common code, actions and more

---
+++ app/src/main/java/com/kennedydias/libermovies/base/BaseFragment.kt
@@ -0,0 +1,111 @@
+package com.kennedydias.libermovies.base
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+
+open class BaseFragment : Fragment(), OnBackPressed {
+
+    private var noConnectionDialogCallback: (() -> Unit)? = null
+    private val noConnectionDialog: AlertDialog? by lazy {
+        context?.let { currentContext ->
+            val builder = AlertDialog.Builder(currentContext)
+            builder.setTitle("No Connection")
+            builder.setMessage("There is no internet connection. \nCheck it and try again.")
+            builder.setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+                noConnectionDialogCallback?.invoke()
+            }
+            builder.create()
+        }
+    }
+    private val snackBarLayout: View by lazy {
+        (activity?.findViewById<View>(android.R.id.content) as ViewGroup).getChildAt(0)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed(): Boolean {
+        return false
+    }
+
+    protected fun invalidateOptionsMenu() {
+        activity?.invalidateOptionsMenu()
+    }
+
+    protected fun finish() {
+        activity?.finish()
+    }
+
+    protected fun setResult(result: Int) {
+        activity?.setResult(result)
+    }
+
+    protected fun setResult(result: Int, intent: Intent) {
+        activity?.setResult(result, intent)
+    }
+
+    protected fun setSupportActionBar(toolbar: Toolbar) {
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+    }
+
+    protected fun setTitle(titleId: Int) {
+        (activity as? AppCompatActivity)?.supportActionBar?.setTitle(titleId)
+    }
+
+    protected fun setTitle(title: String) {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = title
+    }
+
+    protected fun showBackButton() {
+        (activity as? AppCompatActivity)?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun showNoConnectionDialog(callback: (() -> Unit)? = null) {
+        noConnectionDialogCallback = callback
+        if (noConnectionDialog?.isShowing == false) {
+            noConnectionDialog?.show()
+        }
+    }
+
+    fun hideNotConnectedDialog() {
+        if (noConnectionDialog?.isShowing == true) {
+            noConnectionDialog?.dismiss()
+        }
+    }
+
+    fun showSnackbar(message: String) {
+        Snackbar.make(snackBarLayout, message, Snackbar.LENGTH_LONG).apply {
+            setAction("Ok") {
+                dismiss()
+            }
+            show()
+        }
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ app/src/main/java/com/kennedydias/libermovies/base/OnBackPressed.kt
@@ -0,0 +1,10 @@
+package com.kennedydias.libermovies.base
+
+interface OnBackPressed {
+    /**
+     * If you return true the back press will not be taken into account, otherwise the activity will act naturally
+     * So, basically, returning simulates calling super.onBackPressed()
+     * @return true if your processing has priority if not false
+     */
+    fun onBackPressed(): Boolean
+}
